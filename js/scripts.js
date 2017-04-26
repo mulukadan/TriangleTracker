4da3e70c53c737804f9fb2e7dfe415cdf241988f
mulukadan <kadan.md@gmail.com>
2017-04-27 00:00:30 +0300

    done

--- js/scripts.js
+++ js/scripts.js
@@ -23,8 +23,25 @@ $(document).ready(function() {
         }
       }
       }
+        setImageSource(results);
         $(".output").text(results);
 
         event.preventDefault();
       });
+
+      function setImageSource(results) {
+      var imageSrc="";
+      if (results === "Equilateral") {
+         imageSrc="img/e.png";
+      }else if (results === "Isosceles") {
+         imageSrc="img/i.png";
+      }else if (results === "Scalene") {
+         imageSrc="img/s.png";
+      }else {
+        imageSrc="img/n.gif";
+      }
+
+        $('#tri').attr('src', imageSrc);
+      
+      }
     });
